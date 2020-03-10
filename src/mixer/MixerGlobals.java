e4e674bbfb7d771354021f1e5a822c1adc44f867
Muhammad Saad Shamim <sa501428@gmail.com>
2020-03-10 16:38:09 -0500

    code cleanup

--- src/mixer/MixerGlobals.java
+++ src/mixer/MixerGlobals.java
@@ -24,32 +24,15 @@
 
 package mixer;
 
-import java.awt.*;
-
 /**
  * @author Muhammad Shamim
  * @since 11/25/14
  */
 public class MixerGlobals {
 
-    // Juicebox version (for display and header purposes only)
-    public static final String versionNum = "2.02.02";
-
-    // for plotting
-    public static final String topChromosomeColor = "#0000FF";
-    public static final String leftChromosomeColor = "#009900";
-    // Feature2D hover text
-    public static final boolean allowSpacingBetweenFeatureText = true;
-    // min hic file version supported
+    public static final String versionNum = "2.02.03";
     public static final int minVersion = 6;
     public static final int bufferSize = 2097152;
-
-    public static final String defaultPropertiesURL = "http://hicfiles.tc4ga.com/juicebox.properties";
-    public static final Color SELECT_FEATURE_COLOR = Color.DARK_GRAY;
-    public static int MAX_PEARSON_ZOOM = 500000;
-
-    // implement Map scaling with this global variable
-    public static double hicMapScale = 1;
 
     // whether MatrixZoomData should cache or not
     public static boolean useCache = true;
@@ -65,11 +48,5 @@ public class MixerGlobals {
             throw new RuntimeException("This file is version " + version +
                     ". Only versions " + minVersion + " and greater are supported at this time.");
         }
-    }
-
-    public static Font font(int size, boolean isBold) {
-        if (isBold)
-            return new Font("Arial", Font.BOLD, size);
-        return new Font("Arial", Font.PLAIN, size);
     }
 }
--- src/mixer/track/feature/Feature2D.java
+++ src/mixer/track/feature/Feature2D.java
@@ -31,8 +31,6 @@ import mixer.data.anchor.MotifAnchor;
 import mixer.tools.utils.mixer.arrowhead.ArrowheadScore;
 
 import java.awt.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.List;
 import java.util.*;
 
@@ -47,14 +45,11 @@ public class Feature2D implements Comparable<Feature2D> {
     static final String genericHeader = "#chr1\tx1\tx2\tchr2\ty1\ty2\tname\tscore\tstrand1\tstrand2\tcolor";
     private static final String genericLegacyHeader = "#chr1\tx1\tx2\tchr2\ty1\ty2\tcolor";
     private static final String BEDPE_SPACER = "\t.\t.\t.\t.";
-    private static final String[] categories = new String[]{"observed", "coordinate", "enriched", "expected", "fdr"};
     public static int tolerance = 0;
-    public static boolean allowHiCCUPSOrdering = false;
     final FeatureType featureType;
     final Map<String, String> attributes;
     final String chr1;
     final String chr2;
-    private final NumberFormat formatter = NumberFormat.getInstance();
     final int start1;
     final int start2;
     int end1;
@@ -62,7 +57,6 @@ public class Feature2D implements Comparable<Feature2D> {
     private boolean isSelected = false;
     private Feature2D reflection = null;
     private Color color, translucentColor;
-    private boolean test = false;
 
     public Feature2D(FeatureType featureType, String chr1, int start1, int end1, String chr2, int start2, int end2, Color c,
                      Map<String, String> attributes) {
@@ -160,11 +154,7 @@ public class Feature2D implements Comparable<Feature2D> {
     }
 
     public Color getColor() {
-        if (isSelected) {
-            return MixerGlobals.SELECT_FEATURE_COLOR;
-        } else {
-            return color;
-        }
+        return color;
     }
 
 
@@ -175,120 +165,10 @@ public class Feature2D implements Comparable<Feature2D> {
         setTranslucentColor();
     }
 
-    public Color getTranslucentColor() {
-        if (isSelected) {
-            return MixerGlobals.SELECT_FEATURE_COLOR;
-        } else {
-            return translucentColor;
-        }
-    }
-
     private void setTranslucentColor() {
         translucentColor = new Color(color.getRed(), color.getGreen(), color.getBlue(), 50);
         if (reflection != null)
             reflection.translucentColor = translucentColor;
-    }
-
-    public String tooltipText() {
-
-        String scaledStart1 = formatter.format(start1 * MixerGlobals.hicMapScale + 1);
-        String scaledStart2 = formatter.format(start2 * MixerGlobals.hicMapScale + 1);
-        String scaledEnd1 = formatter.format(end1 * MixerGlobals.hicMapScale);
-        String scaledEnd2 = formatter.format(end2 * MixerGlobals.hicMapScale);
-
-        StringBuilder txt = new StringBuilder();
-        txt.append("<span style='color:red; font-family: arial; font-size: 12pt;'>");
-        txt.append(getFeatureName());
-        txt.append("</span><br>");
-
-        txt.append("<span style='font-family: arial; font-size: 12pt;color:" + MixerGlobals.topChromosomeColor + ";'>");
-        txt.append(chr1).append(":").append(scaledStart1);
-        if ((end1 - start1) > 1) {
-            txt.append("-").append(scaledEnd1);
-        }
-
-        txt.append("</span><br>");
-
-        txt.append("<span style='font-family: arial; font-size: 12pt;color:" + MixerGlobals.leftChromosomeColor + ";'>");
-        txt.append(chr2).append(":").append(scaledStart2);
-        if ((end2 - start2) > 1) {
-            txt.append("-").append(scaledEnd2);
-        }
-        txt.append("</span>");
-        DecimalFormat df = new DecimalFormat("#.##");
-
-        if (MixerGlobals.allowSpacingBetweenFeatureText) {
-            // organize attributes into categories. +1 is for the leftover category if no keywords present
-            ArrayList<ArrayList<Map.Entry<String, String>>> sortedFeatureAttributes = new ArrayList<>();
-            for (int i = 0; i < categories.length + 1; i++) {
-                sortedFeatureAttributes.add(new ArrayList<Map.Entry<String, String>>());
-            }
-
-            // sorting the entries, also filtering out f1-f5 flags
-            for (Map.Entry<String, String> entry : attributes.entrySet()) {
-                String tmpKey = entry.getKey();
-                boolean categoryHasBeenAssigned = false;
-                for (int i = 0; i < categories.length; i++) {
-                    if (tmpKey.contains(categories[i])) {
-                        sortedFeatureAttributes.get(i).add(entry);
-                        categoryHasBeenAssigned = true;
-                        break;
-                    }
-                }
-                if (!categoryHasBeenAssigned) {
-                    sortedFeatureAttributes.get(categories.length).add(entry);
-                }
-            }
-
-            // append to tooltip text, but now each category is spaced apart
-            for (ArrayList<Map.Entry<String, String>> attributeCategory : sortedFeatureAttributes) {
-                if (attributeCategory.isEmpty())
-                    continue;
-                //sort attributes before printing
-                Comparator<Map.Entry<String, String>> cmp = new Comparator<Map.Entry<String, String>>() {
-                    @Override
-                    public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2) {
-                        return o1.getKey().compareToIgnoreCase(o2.getKey());
-                    }
-                };
-                Collections.sort(attributeCategory, cmp);
-                for (Map.Entry<String, String> entry : attributeCategory) {
-                    String tmpKey = entry.getKey();
-                    txt.append("<br>");
-                    txt.append("<span style='font-family: arial; font-size: 12pt;'>");
-                    txt.append(tmpKey);
-                    txt.append(" = <b>");
-                    try {
-                        txt.append(df.format(Double.valueOf(entry.getValue())));
-                    } catch (Exception e) {
-                        txt.append(entry.getValue()); // for text i.e. non-decimals
-                    }
-                    txt.append("</b>");
-                    txt.append("</span>");
-                }
-                txt.append("<br>"); // the extra spacing between categories
-            }
-        } else {
-            // simple text dump for plotting, no spacing or rearranging by category
-            for (Map.Entry<String, String> entry : attributes.entrySet()) {
-                String tmpKey = entry.getKey();
-                if (!(tmpKey.equals("f1") || tmpKey.equals("f2") || tmpKey.equals("f3") || tmpKey.equals("f4") || tmpKey.equals("f5"))) {
-                    txt.append("<br>");
-                    txt.append("<span style='font-family: arial; font-size: 12pt;'>");
-                    txt.append(tmpKey);
-                    txt.append(" = <b>");
-                    //System.out.println(entry.getValue());
-                    try {
-                        txt.append(df.format(Double.valueOf(entry.getValue())));
-                    } catch (Exception e) {
-                        txt.append(entry.getValue());
-                    }
-                    txt.append("</b>");
-                    txt.append("</span>");
-                }
-            }
-        }
-        return txt.toString();
     }
 
     public String getOutputFileHeader() {
@@ -465,10 +345,6 @@ public class Feature2D implements Comparable<Feature2D> {
         return hash;
     }
 
-    public void doTest() {
-        test = true;
-    }
-
     public void clearAttributes() {
         attributes.clear();
     }
@@ -495,10 +371,6 @@ public class Feature2D implements Comparable<Feature2D> {
             attrClone.put(key, attributes.get(key));
         }
         return new Feature2D(featureType, chr1, start1, end1, chr2, start2, end2, color, attrClone);
-    }
-
-    public void setSetIsSelectedColorUpdate(boolean setIsSelectedColorUpdate) {
-        isSelected = setIsSelectedColorUpdate;
     }
 
     public Feature2DWithMotif toFeature2DWithMotif() {
