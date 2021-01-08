2b223de75cab5512d7ef26005b1933a5808903e8
Muhammad S Shamim <sa501428@gmail.com>
2021-01-08 15:53:03 -0600

    extracting local subcompartment

--- src/mixer/clt/AggregateProcessing.java
+++ src/mixer/clt/AggregateProcessing.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2011-2020 Rice University, Baylor College of Medicine, Aiden Lab
+ * Copyright (c) 2011-2021 Rice University, Baylor College of Medicine, Aiden Lab
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,7 @@
 
 package mixer.clt;
 
-import mixer.MixerTools;
+import mixer.utils.custom.ExtractionB4;
 
 
 /**
@@ -36,6 +36,9 @@ public class AggregateProcessing {
 
     public static void main(String[] argv) throws Exception {
 
+        ExtractionB4.extract();
+
+        /*
         String[] strings;
 
         String refs = "/Users/mss/Desktop/SLICE.work/subcompartment_analysis/slice/existing/GSE63525_GM12878_subcompartments.bed," +
@@ -237,5 +240,7 @@ public class AggregateProcessing {
         System.out.println("-----------------------------------------------------");
         MixerTools.main(strings);
         System.gc();
+
+         */
     }
 }
