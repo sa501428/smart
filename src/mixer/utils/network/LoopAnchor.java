cd6fdebfdfe71298a67f4e453956f7e545e90eb7
Muhammad S Shamim <sa501428@gmail.com>
2021-03-24 16:55:29 -0500

    network analysis and loop fine-tuning

--- src/mixer/utils/network/LoopAnchor.java
+++ src/mixer/utils/network/LoopAnchor.java
@@ -22,14 +22,44 @@
  *  THE SOFTWARE.
  */
 
-package mixer;
+package mixer.utils.network;
 
-/**
- * @author Muhammad Shamim
- * @since 11/25/14
- */
-public class MixerGlobals {
-    public static final String versionNum = "3.20.01";
-    public static final int bufferSize = 2097152;
-    public static boolean printVerboseComments = false;
+import java.util.*;
+
+public class LoopAnchor {
+    private final List<Integer> connectedLoops = new ArrayList<>();
+    private final int buffer;
+    private long position;
+
+    public LoopAnchor(long midBin, int id, int buffer) {
+        connectedLoops.add(id);
+        position = midBin;
+        this.buffer = buffer;
+    }
+
+
+    public boolean overlaps(long midBin) {
+        return Math.abs(midBin - position) < buffer;
+    }
+
+    public void add(long midBin, int id) {
+        long currentTotal = position * connectedLoops.size();
+        currentTotal += midBin;
+        connectedLoops.add(id);
+        position = currentTotal / connectedLoops.size();
+    }
+
+    public int size() {
+        return connectedLoops.size();
+    }
+
+    public void populateAdjacencyMatrix(Map<Integer, Set<Integer>> adjacencyMatrix) {
+        for (int i : connectedLoops) {
+            if (adjacencyMatrix.containsKey(i)) {
+                adjacencyMatrix.get(i).addAll(connectedLoops);
+            } else {
+                adjacencyMatrix.put(i, new HashSet<>(connectedLoops));
+            }
+        }
+    }
 }
