9745248e7ee05a5029ef97666d7774930d4f867d
Muhammad S Shamim <sa501428@gmail.com>
2021-04-07 12:45:56 -0500

    version number

--- src/mixer/MixerGlobals.java
+++ src/mixer/MixerGlobals.java
@@ -29,7 +29,7 @@ package mixer;
  * @since 11/25/14
  */
 public class MixerGlobals {
-    public static final String versionNum = "3.20.13";
+    public static final String versionNum = "3.21.01";
     public static final int bufferSize = 2097152;
     public static boolean printVerboseComments = false;
 }
