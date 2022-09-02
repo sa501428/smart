89b0fc24c3a50f6afa0cc2ee801971753ad9ce6a
Muhammad Saad Shamim <sa501428@gmail.com>
2022-09-01 23:45:15 -0500

    update version

--- src/mixer/SmartTools.java
+++ src/mixer/SmartTools.java
@@ -37,7 +37,7 @@ import mixer.clt.MixerCLT;
  */
 public class SmartTools {
 
-    public static final String versionNum = "0.3.0";
+    public static final String versionNum = "0.3.2";
     public static final int bufferSize = 2097152;
     public static boolean printVerboseComments = false;
 
